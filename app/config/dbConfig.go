b0983cd898c6e13f8cec30bcb07df83d9962adcd
rkrish82 <ramakrishna.srinivas.murthy@gmail.com>
2019-01-19 20:43:52 +0530

    Initial commit with directory structure of the project
    Signed-off-by: rkrish82 <ramakrishna.srinivas.murthy@gmail.com>

---
+++ app/config/dbConfig.go
@@ -0,0 +1,103 @@
+package config
+
+import (
+	"github.com/globalsign/mgo"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	url  = "localhost"
+	port = "27017"
+	db   = "intelliQ"
+)
+
+var dbSession *mgo.Session
+
+//Connect db conn
+func Connect() (*mgo.Session, error) {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	log.Info("Successfully connected to DB at ", url)
+	dbSession = session
+	//	createIndices(session.Copy())
+	return session, nil
+}
+
+//GetCollection copy of original session
+func GetCollection(dbName string, collName string) *mgo.Collection {
+	if dbSession == nil {
+		return nil
+	}
+	session := dbSession.Copy()
+	db := session.DB(dbName)
+	if db == nil {
+		return nil
+	}
+	coll := db.C(collName)
+	if coll == nil {
+		return nil
+	}
+	return coll
+}
+
+type searchField struct {
+	field  string
+	weight int
+}
+
+func createIndices(session *mgo.Session) {
+	db := session.DB(db)
+	if db == nil {
+		panic("No DB session")
+	}
+
+	var searchFields []searchField
+	searchFields = append(searchFields, searchField{field: "city", weight: 4})
+	searchFields = append(searchFields, searchField{field: "state", weight: 2})
+
+	addSearchIndex(db, "addresses", searchFields)
+	addUniqueIndex(db, "addresses", []string{"city"})
+	db.Session.Close()
+}
+
+func addSearchIndex(db *mgo.Database, collName string, searchFields []searchField) {
+	coll := db.C(collName)
+	if coll == nil {
+		panic("No such Collection in DB" + collName)
+	}
+	var fields []string
+	weights := make(map[string]int)
+
+	for _, val := range searchFields {
+		fields = append(fields, "$text:"+val.field)
+		weights[val.field] = val.weight
+	}
+	index := mgo.Index{
+		Key:     fields,
+		Weights: weights,
+		Name:    "textIndex",
+	}
+	err := coll.EnsureIndex(index)
+	if err != nil {
+		panic("Hi Could not create search index for " + collName + err.Error())
+	}
+}
+
+func addUniqueIndex(db *mgo.Database, collName string, fields []string) {
+	coll := db.C(collName)
+	if coll == nil {
+		panic("No such Collection in DB" + collName)
+	}
+	for _, key := range fields {
+		index := mgo.Index{
+			Key:    []string{key},
+			Unique: true,
+		}
+		if err := coll.EnsureIndex(index); err != nil {
+			panic("Could not create unique index for " + collName)
+		}
+	}
+}
---
+++ app/model/responseModel.go
@@ -0,0 +1,8 @@
+package model
+
+//AppResponse respone
+type AppResponse struct {
+	Status int
+	Body   interface{}
+	Msg    string
+}
