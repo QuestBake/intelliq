5a74edb1be9afcf3b8febdaf5483e23e3aae8d36
Lionheart <raghavaneja92@gmail.com>
2019-02-05 20:57:30 +0530

    replaced user object id with newly local model admin to prevent circular import error in GO

--- app/model/groupModel.go
+++ app/model/groupModel.go
@@ -12,9 +12,18 @@ type Group struct {
 	Code               string        `json:"code" bson:"code"`
 	CollectionName     string        `json:"collName" bson:"collName"`
 	QuestionCategories []string      `json:"quesCategories" bson:"quesCategories"`
-	Admin              bson.ObjectId `json:"admin" bson:"admin"`
+	Admin              admin         `json:"admin" bson:"admin"`
 	Subjects           []Subject     `json:"subjects" bson:"subjects"`
 	AuxQuestions       []Question    `json:"auxQuestions" bson:"auxQuestions"`
 	CreateDate         time.Time     `json:"createDate" bson:"createDate"`
 	LastModifiedDate   time.Time     `json:"lastModifiedDate" bson:"lastModifiedDate"`
+}
+
+type admin struct {
+	UserID   bson.ObjectId `json:"userId" bson:"_id"`
+	FullName string        `json:"name" bson:"name"`
+	Gender   string        `json:"gender" bson:"gender"`
+	Mobile   string        `json:"mobile" bson:"mobile"`
+	Email    string        `json:"email" bson:"email"`
+	School   string        `json:"schoolName" bson:"schoolName"`
 }
