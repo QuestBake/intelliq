ab8d5a0f12a35f1e31927515bbea81e469b77c7e
Lionheart <raghavaneja92@gmail.com>
2019-02-09 19:53:42 +0530

    added null check while reading request params

--- app/controller/metaController.go
+++ app/controller/metaController.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"intelliq/app/common"
+	utility "intelliq/app/common"
 	"intelliq/app/model"
 	"intelliq/app/service"
 	"net/http"
@@ -8,24 +10,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var res *model.AppResponse
+
 //AddMetaData adds meta data
 func AddMetaData(ctx *gin.Context) {
 	var metaData model.Meta
-	ctx.BindJSON(&metaData)
-	res := service.AddNewData(&metaData)
+	err := ctx.BindJSON(&metaData)
+	if err != nil {
+		res = utility.GetErrorResponse(common.MSG_BAD_INPUT)
+	} else {
+		res = service.AddNewData(&metaData)
+	}
 	ctx.JSON(http.StatusOK, res)
 }
 
 //UpdateMetaData updates meta data
 func UpdateMetaData(ctx *gin.Context) {
 	var metaData model.Meta
-	ctx.BindJSON(&metaData)
-	res := service.UpdateMetaData(&metaData)
+	err := ctx.BindJSON(&metaData)
+	if err != nil {
+		res = utility.GetErrorResponse(common.MSG_BAD_INPUT)
+	} else {
+		res = service.UpdateMetaData(&metaData)
+	}
 	ctx.JSON(http.StatusOK, res)
 }
 
 //ReadMetaData fetches meta data
 func ReadMetaData(ctx *gin.Context) {
-	res := service.ReadMetaData()
+	res = service.ReadMetaData()
 	ctx.JSON(http.StatusOK, res)
 }
